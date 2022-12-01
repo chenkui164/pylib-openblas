61e41f639a90869b2dafc69049c5dfc95a08fed1
chenkui164 <chenkui164@163.com>
2022-12-01 11:48:02 +0800

    aaa

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class MyBuildCLib(build_clib):
                                    os.path.join(
                                        cwd, 'OpenBLAS-{version}'.format(version=OpenBLASVersion)),
                                    "-DCMAKE_INSTALL_PREFIX=" + install_prefix])
-            subprocess.check_call("make -j2")
+            subprocess.check_call(['make', '-j2'])
             subprocess.check_call(
                 ["cmake", "--build", '.', '--target', 'install'])
 
@@ -84,66 +84,33 @@ class MyBuildCLib(build_clib):
 
             print("Downloading OpenBLAS version {} arch {}".format(
                 OpenBLASVersion, arch))
-            # request.urlretrieve(
-            #     "https://github.com/xianyi/OpenBLAS/releases/download/v{version}/OpenBLAS-{version}-{tarch}.zip".format(version=OpenBLASVersion, tarch=arch), fname_zip)
+            request.urlretrieve(
+                "https://github.com/xianyi/OpenBLAS/releases/download/v{version}/OpenBLAS-{version}-{tarch}.zip".format(version=OpenBLASVersion, tarch=arch), fname_zip)
 
-            # os.makedirs(self.build_temp)
             cwd = os.getcwd()
             guess_libplat = glob.glob(os.path.join(cwd, 'build', 'lib*'))[0]
             install_dir = os.path.join(guess_libplat, 'pylib_openblas')
-            # os.makedirs(install_dir)
 
             import zipfile
             print("Extracting OpenBLAS version {}".format(OpenBLASVersion))
             with zipfile.ZipFile(fname_zip, 'r') as zip_ref:
                 zip_ref.extractall(install_dir)
 
-            # import subprocess
-            # print("Building OpenBLAS version {}".format(OpenBLASVersion))
-            # dynamic_arch = int(platform != "win32")
-            # if platform == "win32":
-            #     dynamic_arch = 0
-            #     builder = ["cmake", "--build", '.']
-            # else:
-            #     dynamic_arch = 1
-            #     builder = ['make', '-j2']
-
-            # try:
-            #     os.makedirs(self.build_temp)
-            # except OSError:
-            #     pass
-
-            # os.chdir(self.build_temp)
-            # # this is clumsy <3
-            # guess_libplat = glob.glob(os.path.join(cwd, 'build', 'lib*'))[0]
-            # install_prefix = os.path.join(guess_libplat, 'pylib_openblas')
-            # subprocess.check_call(["cmake",
-            #                        '-DCMAKE_BUILD_TYPE=Release',
-            #                        '-DDYNAMIC_ARCH=1',
-            #                        '-DNOFORTRAN=1',
-            #                        '-DNO_LAPACK=1',
-            #                        '-DBUILD_SHARED_LIBS=OFF',
-            #                        os.path.join(
-            #                            cwd, 'OpenBLAS-{version}'.format(version=OpenBLASVersion)),
-            #                        "-DCMAKE_INSTALL_PREFIX=" + install_prefix])
-            # subprocess.check_call(builder)
-            # subprocess.check_call(
-            #     ["cmake", "--build", '.', '--target', 'install'])
-
-            guess_libblas = glob.glob(os.path.join(
-                install_dir, 'lib', '*lib*'))[0]
-            print('guess_libblas is {}'.format(guess_libblas))
-            # target_libblas = guess_libblas.replace(
-            #     'openblas', 'pylib_openblas')
-            # copyfile(guess_libblas, os.path.basename(target_libblas))
-            libplacehoder = os.path.join(self.build_temp, "lib" + name + ".a")
-            with open(libplacehoder, 'w') as fp:
+            try:
+                os.makedirs(self.build_temp)
+            except OSError:
                 pass
 
-            print('libplacehoder is {}'.format(libplacehoder))
+            guess_libblas = glob.glob(os.path.join(
+                install_dir, 'lib', '*.lib'))[0]
+            print('guess_libblas is {}'.format(guess_libblas))
+            libplacehoder = os.path.join(self.build_temp, name + ".lib")
+            copyfile(guess_libblas, libplacehoder)
 
-            # os.chdir(cwd)
-        build4win()
+        if platform == "win32":
+            build4win()
+        else:
+            build4linux()
 
 
 setup(name=name,
